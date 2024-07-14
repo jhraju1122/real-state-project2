da80e2ee0a4348bf26b7e1b8d9dfac6f74c5d1b6
jhraju <jhraju890@gmail.com>
2024-07-14 11:34:26 +0600

    added error page

---
+++ src/Errorpage/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+               <h2>Oops!! SORRY  Raju mama!</h2>
+            <Link to="/">Go back to home</Link>
+        </div>
+    );
+};
+
+export default ErrorPage;
